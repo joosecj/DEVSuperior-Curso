08266bb9937fcdb655ab5e5bded1389288fa5a5b
joosecj <josecarloscjj@gmail.com>
2022-06-14 19:52:36 -0300

    save exercicioUm java

--- 3-Programacao-Moderna/7-Herança_Polimorfismo/Exercicios/ExercicioUm/src/application/Program.java
+++ 3-Programacao-Moderna/7-Herança_Polimorfismo/Exercicios/ExercicioUm/src/application/Program.java
@@ -1,7 +1,54 @@
 package application;
 
+import entities.Employee;
+import entities.OutsourceEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.print("Enter the number of employees: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Employee #" + i + " data: ");
+            System.out.print("Outsourced (y/n)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Hours: ");
+            int hours = sc.nextInt();
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+            if ( ch == 'y') {
+                System.out.print("Additional charge: ");
+                double additionalCharge = sc.nextDouble();
+                //posso colocar dentro do add.
+                list.add(new OutsourceEmployee(name, hours, valuePerHour, additionalCharge));
+            } else {
+                // exemplo acima
+                Employee emp = new Employee(name, hours, valuePerHour);
+                list.add(emp);
+            }
+        }
+
+        System.out.println();
+        System.out.println("PAYMENTS");
+        for (Employee emp : list){
+            System.out.println(emp.getName() + " - $ " + String.format("%.2f", emp.payment()));
+        }
+
+
+        sc.close();
 
     }
 }
