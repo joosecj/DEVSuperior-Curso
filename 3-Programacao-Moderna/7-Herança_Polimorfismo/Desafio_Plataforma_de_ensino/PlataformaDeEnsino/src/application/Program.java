7296499e1fd4bbcd65ce20dd72b6fae0305cedb2
joosecj <josecarloscjj@gmail.com>
2022-06-16 21:36:06 -0300

    finish class Program

--- 3-Programacao-Moderna/7-Herança_Polimorfismo/Desafio_Plataforma_de_ensino/PlataformaDeEnsino/src/application/Program.java
+++ 3-Programacao-Moderna/7-Herança_Polimorfismo/Desafio_Plataforma_de_ensino/PlataformaDeEnsino/src/application/Program.java
@@ -1,5 +1,11 @@
 package application;
 
+import entities.Lesson;
+import entities.Task;
+import entities.Video;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,7 +14,40 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        List<Lesson> lessonList = new ArrayList<>();
 
+        System.out.print("Quants aulas tem o curso? ");
+        int n = sc.nextInt();
+        System.out.println();
+        for(int i = 1; i <= n; i++){
+            System.out.println("Dados da " + i +"º aula:");
+            System.out.print("Conteúdo ou tarefa (c/t)?");
+            char ch = sc.next().charAt(0);
+            System.out.print("Título: ");
+            String title = sc.nextLine();
+            if (ch == 'c'){
+                System.out.print("URL do vídeo: ");
+                String url = sc.nextLine();
+                System.out.print("Duração em segundos: ");
+                int seconds = sc.nextInt();
+                lessonList.add(new Video(title, url, seconds));
+
+            }else {
+                System.out.print("Descrição: ");
+                String description = sc.nextLine();
+                System.out.print("Quantidade de questões: ");
+                int question = sc.nextInt();
+                lessonList.add(new Task(title, description, question));
+            }
+        System.out.println();
+        }
+
+        System.out.println();
+        int sum = 0;
+        for(Lesson list : lessonList){
+            sum += list.durantion();
+        }
+        System.out.println("DURAÇÃO TOTAL DO CURSO = " + sum + " segundos");
 
         sc.close();
     }
