d092de06f74bac0e838699c70c755a74c7f8da39
joosecj <josecarloscjj@gmail.com>
2022-06-18 22:52:35 -0300

    exercise finished

---
+++ 3-Programacao-Moderna/10-Interfaces/ExercicioInterface2/src/modal/services/ContractService.java
@@ -0,0 +1,32 @@
+package modal.services;
+
+import modal.entities.Contract;
+import modal.entities.Installment;
+
+import java.util.Calendar;
+import java.util.Date;
+public class ContractService {
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(){
+    }
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+    public void processContract(Contract contract, Integer months){
+        double basicQuota = contract.getTotalValue() / months;
+        for(int i = 1; i <= months; i++){
+            Date date =  addMonths(contract.getDate(), i);
+            double updateQuota = basicQuota + onlinePaymentService.interest(basicQuota, i);
+            double fullQuota = updateQuota + onlinePaymentService.paymentFee(updateQuota);
+            contract.addInstallment(new Installment(date, fullQuota));
+        }
+    }
+    private Date addMonths(Date date, int n) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.MONTH, n);
+        return cal.getTime();
+    }
+
+}
